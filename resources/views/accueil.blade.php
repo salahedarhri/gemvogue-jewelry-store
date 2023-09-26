b2e643a0b17dea2f149bf3f87d4813399835fec9
salahedarhri <arhri.salah@gmail.com>
2023-09-26 19:30:59 +0100

    changed landing page

--- resources/views/accueil.blade.php
+++ resources/views/accueil.blade.php
@@ -3,8 +3,33 @@
 @section('content')
 
 <!-- Carrousel -->
-
-
+<div class="flex justify-center items-center m-5">
+  <div class="carousel w-screen">
+    <div id="slide1" class="carousel-item relative w-full">
+      <img src="/images/bg-bracelet.jpg" class="w-full" />
+      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide3" class="btn btn-circle">❮</a> 
+        <a href="#slide2" class="btn btn-circle">❯</a>
+      </div>
+    </div> 
+    <div id="slide2" class="carousel-item relative w-full">
+      <p>du coup</p>
+      <img src="/images/bg-ring.jpg" class="w-full" />
+      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide1" class="btn btn-circle">❮</a> 
+        <a href="#slide3" class="btn btn-circle">❯</a>
+      </div>
+    </div> 
+    <div id="slide3" class="carousel-item relative w-full">
+      <p>ça marche</p>
+      <img src="/images/bg-collier.jpg" class="w-full" />
+      <div class="absolute flex justify-between transform -translate-y-1/2 left-5 right-5 top-1/2">
+        <a href="#slide2" class="btn btn-circle">❮</a> 
+        <a href="#slide1" class="btn btn-circle">❯</a>
+      </div>
+    </div> 
+  </div>
+</div>
 <!-- New Collection Hero -->
 <div class="hero min-h-min h-96 max-sm:h-80 " style="background-image: url(images/bg-hero.jpg);">
   <div class="hero-overlay bg-opacity-70"></div>
