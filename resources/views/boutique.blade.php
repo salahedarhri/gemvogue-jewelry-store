e0cd6dd3a6535e84ea78b4d7229b1399f5a1c4e4
salahedarhri <arhri.salah@gmail.com>
2023-10-01 15:12:50 +0100

    fixing mobile format display

--- resources/views/boutique.blade.php
+++ resources/views/boutique.blade.php
@@ -5,7 +5,7 @@
 <div class="lg:mx-24 xl:mx-48">
 
   <!-- Section Anneau -->
-<div class="hero min-h-min h-48 shadow-md" style="background-image: url(images/bg-ring.jpg);">
+<div class="hero h-48 max-sm:h-28 shadow-md" style="background-image: url(images/bg-ring.jpg);">
   <div class="hero-overlay bg-yellow-800 bg-opacity-70"></div>
   <div class="hero-content text-center text-white ">
     <div class="max-w-md">
@@ -15,7 +15,7 @@
 </div>
 
 
-<div class="grid gap-2 p-6 place-items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+<div class="grid gap-2 max-sm:gap-0 p-6 max-sm:p-2 place-items-center max-sm:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
   @foreach ($bijoux as $bijou)
 
     @if ( $bijou->type =='Anneau')
@@ -25,7 +25,7 @@
         <figure class="px-2 pt-2">
           <img src="images/{{ $bijou->photo1 }}"
           alt="img bijou database"
-          class="rounded-lg h-48 w-48 object-cover">
+          class="rounded-lg h-48 w-48 max-sm:h-36 max-sm:w-36 object-cover">
         </figure>
 
         <h2 class=" font-semibold  p-0.5">{{ $bijou->nom }}</h2>
@@ -40,7 +40,7 @@
 </div>
 
 <!-- Section Collier -->
-<div class="hero min-h-min h-48 mt-8 shadow-md" style="background-image: url(images/bg-collier.jpg);">
+<div class="hero h-48 max-sm:h-28 mt-8 shadow-md" style="background-image: url(images/bg-collier.jpg);">
   <div class="hero-overlay bg-amber-800 bg-opacity-70"></div>
   <div class="hero-content text-center text-white">
     <div class="max-w-md">
@@ -49,7 +49,7 @@
   </div>
 </div>
 
-<div class="grid gap-2 p-6 place-items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+<div class="grid gap-2 max-sm:gap-0 p-6 max-sm:p-2  place-items-center  max-sm:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
 
   @foreach ($bijoux as $bijou)
 
@@ -60,7 +60,7 @@
         <figure class="px-2 pt-2">
           <img src="images/{{ $bijou->photo1 }}"
           alt="img bijou database"
-          class="rounded-lg h-48 w-48 object-cover">
+          class="rounded-lg h-48 w-48 max-sm:h-36 max-sm:w-36 object-cover">
         </figure>
 
         <h2 class=" font-semibold  p-0.5">{{ $bijou->nom }}</h2>
@@ -75,7 +75,7 @@
 </div>
 
 <!-- Section Bracelet -->
-<div class="hero min-h-min h-48 mt-8 shadow-md" style="background-image: url(images/bg-bracelet.jpg);">
+<div class="hero h-48 max-sm:h-28 mt-8 shadow-md" style="background-image: url(images/bg-bracelet.jpg);">
   <div class="hero-overlay bg-yellow-800 bg-opacity-70"></div>
   <div class="hero-content text-center text-white">
     <div class="max-w-md">
@@ -84,7 +84,7 @@
   </div>
 </div>
 
-<div class="grid gap-2 p-6 place-items-center sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
+<div class="grid gap-2 max-sm:gap-0 p-6 max-sm:p-2  place-items-center  max-sm:grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 2xl:grid-cols-6">
 
   @foreach ($bijoux as $bijou)
 
@@ -95,7 +95,7 @@
         <figure class="px-2 pt-2">
           <img src="images/{{ $bijou->photo1 }}"
           alt="img bijou database"
-          class="rounded-lg h-48 w-48 object-cover">
+          class="rounded-lg h-48 w-48 max-sm:h-36 max-sm:w-36 object-cover">
         </figure>
 
         <h2 class=" font-semibold  p-0.5">{{ $bijou->nom }}</h2>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -17,8 +17,8 @@
   <!-- Barre Navigation -->
   <div class="navbar bg-base-100">
     <div class="flex-1 ">
-        <a href="/"><img src="images/ring-logo (2).png" class="h-9 w-auto ml-2" alt="logo"></a>
-        <a href="/" class="font-semibold normal-case text-xl max-sm:text-lg p-2">GemVogue</a>
+        <a href="/"><img src="images/ring-logo (2).png" class="h-9 w-auto ml-2 max-sm:h-7" alt="logo"></a>
+        <a href="/" class="font-semibold normal-case text-xl max-sm:text-md p-2">GemVogue</a>
     </div>
 
     <div class="flex-none">
@@ -40,7 +40,7 @@
       <!-- medium to small screen -->
       <div class="dropdown dropdown-bottom dropdown-end md:hidden">
         <label tabindex="0"><svg xmlns="http://www.w3.org/2000/svg"  viewBox="0 0 50 50" width="150px" height="150px" class="w-8 h-8 m-2 fill-neutral"><path d="M 5 8 A 2.0002 2.0002 0 1 0 5 12 L 45 12 A 2.0002 2.0002 0 1 0 45 8 L 5 8 z M 5 23 A 2.0002 2.0002 0 1 0 5 27 L 45 27 A 2.0002 2.0002 0 1 0 45 23 L 5 23 z M 5 38 A 2.0002 2.0002 0 1 0 5 42 L 45 42 A 2.0002 2.0002 0 1 0 45 38 L 5 38 z"/></svg></label>
-        <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-52 max-sm:w-52 max:sm">
+        <ul tabindex="0" class="dropdown-content z-[1] menu p-2 shadow bg-base-100 rounded-box w-48">
           <li><a href="/" class="text-base" >Accueil</a></li>
           <li><a href="/boutique" class="text-base" >Boutique</a></li>
           <li><a href="/apropos" class="text-base">à propos</a></li>
